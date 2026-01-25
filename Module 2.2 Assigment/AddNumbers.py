d37603b43af2b446f8c6fc954b3ffd298c30d181
miguel <concord808030@gmail.com>
2026-01-25 15:39:42 -0800

    Module 2.2

---
+++ Module 2.2 Assigment/AddNumbers.py
@@ -0,0 +1,15 @@
+#Miguel Fernandez Brazon
+#01/25/2026
+#Module 2.2 Assignment
+def add_numbers(c, d):
+#Add Numbers
+    sum_result = c + d
+    return sum_result # Returns the result
+
+
+number1 = 15
+number2 = 15
+total = add_numbers(numer1, number2)
+
+# Print the final result
+print(f"The sum of {number1} and {number2} is {total}")
